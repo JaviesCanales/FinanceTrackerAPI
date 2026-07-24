9a9db62e0c2bcce5e1e23724993d1923f25a4edb
Javies Canales <javiescanales@Javiess-MacBook-Air.local>
2026-07-24 15:00:42 -0400

    Add login JWT handler and inline success/error messages for register form

--- docs/login.js
+++ docs/login.js
@@ -8,8 +8,8 @@ registerForm.addEventListener("submit", (event) => {
     console.log("Form submitted!")
 
     const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const email = document.getElementById("register-email").value;
+    const password = document.getElementById("register-password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
     const user = {
         name,
@@ -29,9 +29,43 @@ registerForm.addEventListener("submit", (event) => {
     })
     .then(response => response.json())
     .then(data => {
-        console.log("Account created", data);
-        registerForm.reset();
+        const message = document.getElementById("message");
+        if (data.id) {
+            message.textContent = "Account created succesfully!";
+            message.style.color = "green";
+            message.style.display = "block";
+            registerForm.reset();
+        } else {
+            message.textContent = "Registration failed. Email may already be in use";
+            message.style.color = "red";
+            message.style.display = "block";
+        }
     })
     .catch(error => console.error("Error:", error));
+});
+
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    console.log("Log in")
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    fetch(`${API_URL}/api/login`, {
+        method: "POST",
+        headers: {"Content-type": "application/json" },
+        body: JSON.stringify({email, password})
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.token) {
+            localStorage.setItem("token", data.token);
+            window.location.href = "index.html";
+        } else {
+            alert("Invalid email or password")
+        }
+    })
+    .catch(error => console.error("Error", error));
 });
 
